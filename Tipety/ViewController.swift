68cff6c143dc7bd70ea6c6d1ad051abf526f3b7d
Hugo Ahlberg <hugo@melanchology.com>
2015-01-17 19:49:04 -0800

    added gif animation to readme

--- Tipety/ViewController.swift
+++ Tipety/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+ 
+    @IBAction func editingActive(sender: AnyObject) {
+        
+        var tipPercentages = [0.18, 0.2, 0.25]
+        var tipSelected = tipPercentages [tipControl.selectedSegmentIndex]
+        
+        
+    //   var billAmount = billField.text.Float(true)
+    //var billAmount = billField.text._bridgeToObjectiveC().doubleValue
+        
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tip = billAmount * tipSelected
+        var total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+    }
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
 }
+
 
